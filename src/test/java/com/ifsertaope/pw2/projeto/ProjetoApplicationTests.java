cb5de41b1ffe0b1a6d87d6d8f16f4bcac8995a42
vailaandre <andreanderson.ce01@hotmail.com>
2022-12-06 16:15:24 -0300

    Project created

---
+++ src/test/java/com/ifsertaope/pw2/projeto/ProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifsertaope.pw2.projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
